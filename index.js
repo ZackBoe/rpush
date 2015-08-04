328dd61619f59454dfeef04d643b07fd1e160d8b
Zack Boehm <git@zackboehm.com>
2015-08-04 02:42:40 -0400

    init rpush - it does stuff!

---
+++ index.js
@@ -0,0 +1,49 @@
+var chalk = require('chalk');
+var pushbullet = require('pushbullet');
+var rockets = require('rockets');
+var secrets = require('./secrets.json');
+
+var error = chalk.bold.red;
+var info = chalk.dim.blue;
+
+var pusher = new pushbullet(secrets.pushbulletAPI);
+var client = new rockets();
+
+var subs = {
+  'GameDeals': 'rpush-gamedeals',
+  'freebies': 'rpush-freebies',
+  'AppHookup': 'rpush-apphookup'
+};
+
+// Register events on the client.
+client.on('connect', function() {
+  // Subscribe to posts on our configured subreddits
+  client.subscribe('posts', {
+    subreddit: Object.keys(subs)
+  });
+  console.log(info('['+new Date(Date.now()).toTimeString().split(' ')[0]+'] ')+chalk.bgGreen.black(' Connected ')); 
+});
+
+// There's a new post on a subscribed subreddit
+client.on('post', function(post) {
+  var data = post.data;
+  var time = new Date(data.created_utc * 1000).toTimeString().split(' ')[0];
+
+  // Grab the pushbullet channel tag for this subreddit
+  pushParams = {'channel_tag': subs[data.subreddit]};
+
+  // Push the link!
+  pusher.link(pushParams, data.title, data.url, function(err, response){
+    if(err) {
+      console.log(info('['+time+']')+error(' Failed to push ')+chalk.yellow(data.id)+' from ' +chalk.yellow('r/'+data.subreddit)+' to '+chalk.green(subs[data.subreddit]));
+      console.log(err);
+    }
+    else console.log(info('['+time+']')+' Pushed '+chalk.yellow(data.id)+' from ' +chalk.yellow('r/'+data.subreddit)+' to '+chalk.green(subs[data.subreddit]));
+  });
+});
+
+client.on('disconnect', function(){ 
+  console.log(info('['+new Date(Date.now()).toTimeString().split(' ')[0]+'] ')+chalk.bgRed.black(' Disconnected ')); 
+});
+
+client.connect();
